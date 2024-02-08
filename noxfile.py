1555c1785c7778f678b21c5bc5b5ce33639002c2
Avihai Shalom <avihais12344@gmail.com>
2024-02-08 12:02:29 +0200

    Added ruff as formatter and linter for this code.

--- noxfile.py
+++ noxfile.py
@@ -1,21 +1,29 @@
+from io import StringIO
 from pathlib import Path
 
 import nox
 
-_MIN_COVERAGE_PRECENT: int = 90
 _PROJECT_ROOT: Path = Path(__file__).parent.resolve()
-_CODE_FILES: Path = (_PROJECT_ROOT / "tlab").resolve()
-_COVERAGE_FILE: Path = (_PROJECT_ROOT / "coverage").resolve()
 
-# nox.options.force_venv_backend = "none"
+# Coverage
+_COVERAGE_FILE: Path = (_PROJECT_ROOT / "coverage").resolve()
+_CODE_FILES: Path = (_PROJECT_ROOT / "tlab").resolve()
+_MIN_COVERAGE_PRECENT: int = 90
+
+# Ruff
+_RUFF_CONFIG_FILE: Path = (_PROJECT_ROOT / "ruff.toml").resolve()
+_RUFF_OUTPUT_FORMAT: str = "grouped"
+_RUFF_OUTPUT_FILE: Path = (_PROJECT_ROOT / "lint_output.txt").resolve()
+
+
 nox.options.default_venv_backend = "none"
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session
+@nox.session(tags=["test", "ci"])
 def test(
     session: nox.Session,
-):
+) -> None:
     junit_file: Path = _PROJECT_ROOT / "junit.xml"
     session.run(
         "coverage",
@@ -32,13 +40,13 @@ def test(
     )
 
 
-@nox.session
+@nox.session(tags=["test", "ci"])
 def coverage(
     session: nox.Session,
-):
+) -> None:
     html_dir: Path = (_PROJECT_ROOT / "html_cov").resolve()
     xml_cov: Path = (_PROJECT_ROOT / "coverage.xml").resolve()
-    
+
     session.run(
         "coverage",
         "html",
@@ -61,15 +69,32 @@ def coverage(
     )
 
 
-@nox.session
+@nox.session(tags=["lint", "ci"])
 def lint(
     session: nox.Session,
-):
-    pass
+) -> None:
+    session.run(
+        "ruff",
+        "check",
+        f"{_PROJECT_ROOT!s}",
+        f"--config={_RUFF_CONFIG_FILE!s}",
+        f"--output-format={_RUFF_OUTPUT_FORMAT!s}",
+        f"--output-file={_RUFF_OUTPUT_FILE!s}",
+    )
 
 
-@nox.session
-def format(
+@nox.session(tags=["lint", "ci"])
+def check_format(
     session: nox.Session,
-):
-    pass
+) -> None:
+    format_output_file: Path = (_PROJECT_ROOT / "format_output.txt").resolve()
+    with format_output_file.open(mode="w", encoding="UTF8") as output_file:
+        session.run(
+            "ruff",
+            "format",
+            f"--config={_RUFF_CONFIG_FILE!s}",
+            "--check",
+            "--diff",
+            f"{_PROJECT_ROOT!s}",
+            stdout=output_file,
+        )
