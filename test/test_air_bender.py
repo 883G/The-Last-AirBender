a95df37d1dfda21312f36d392b64b157741b3090
Avihai Shalom <avihais12344@gmail.com>
2024-02-09 11:52:45 +0200

    Added tests for the setters, and fixed the imports.

--- test/test_air_bender.py
+++ test/test_air_bender.py
@@ -3,7 +3,7 @@ import sys
 from decimal import Decimal
 
 import pytest
-from hypothesis import given
+from hypothesis import example, given
 from hypothesis import strategies as st
 
 from tlab.air_bender import AirBender
@@ -64,6 +64,73 @@ def test_ctor_requires_power_of_positive_int_and_failes_on_non_int(
         match="Power level must be a positive integer",
     ):
         AirBender("Aang", power)
+
+
+@given(
+    new_name=st.text(),
+)
+def test_name_setter_on_valid_value(new_name: str) -> None:
+    # Arrange.
+    aang = AirBender("Aang", 90)
+
+    # Act.
+    aang.name = new_name
+
+    # Assert.
+    assert aang.name == new_name
+
+
+@given(
+    new_power=POSITIVE_INTEGERS,
+)
+def test_power_setter_on_valid_value(new_power: int) -> None:
+    # Arrange.
+    aang = AirBender("Aang", 90)
+
+    # Act.
+    aang.power = new_power
+
+    # Assert.
+    assert aang.power == new_power
+
+
+@given(
+    new_power=NEGETIVE_INTEGERS,
+)
+def test_power_setter_failes_on_invalid_value(new_power: int) -> None:
+    # Arrange.
+    aang = AirBender("Aang", 90)
+
+    # Act & assert.
+    with pytest.raises(
+        InvalidPowerValueError,
+        match="Power level must be a positive integer",
+    ):
+        aang.power = new_power
+
+
+@given(
+    new_power=st.one_of(
+        st.booleans(),
+        st.floats(),
+        st.decimals(),
+        st.text(),
+    ),
+)
+@example(new_power=True)
+@example(new_power=False)
+def test_power_setter_failes_on_invalid_type(
+    new_power: str | bool | float | Decimal,
+) -> None:
+    # Arrange.
+    aang = AirBender("Aang", 90)
+
+    # Act & assert.
+    with pytest.raises(
+        InvalidPowerTypeError,
+        match="Power level must be a positive integer",
+    ):
+        aang.power = new_power
 
 
 def test_can_use_airbending() -> None:
