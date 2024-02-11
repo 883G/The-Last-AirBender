698d2bb2fdfd2d6ddc492ac39558039627f4de39
Avihai Shalom <avihais12344@gmail.com>
2024-02-11 21:14:04 +0200

    Added skeleton to the other benders.

--- tlab/water_bender.py
+++ tlab/water_bender.py
@@ -1,8 +1,31 @@
-#!/usr/bin/env python3
+class WaterBender:
+    # Return type is None due to: https://peps.python.org/pep-0484/#the-meaning-of-annotations
+    def __init__(
+        self,
+        name: str,
+        power: int,
+    ) -> None:
+        raise NotImplementedError("You Should Implement this method")
 
-from .bender import Bender
+    @property
+    def name(self) -> str:
+        raise NotImplementedError("You Should Implement this method")
 
-class WaterBender(Bender):
-    pass
-    
-        
+    @name.setter
+    def set_name(self, name: str) -> None:
+        raise NotImplementedError("You Should Implement this method")
+
+    @property
+    def power(self) -> int:
+        raise NotImplementedError("You Should Implement this method")
+
+    @power.setter
+    def set_power(self, power: int) -> None:
+        raise NotImplementedError("You Should Implement this method")
+
+    @property
+    def skill(self) -> str:
+        raise NotImplementedError("You Should Implement this method")
+
+    def bend(self) -> None:
+        raise NotImplementedError("You Should Implement this method")
