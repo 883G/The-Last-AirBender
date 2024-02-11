047c6b42520484b4dbd5dbe35ccafdeb29784b90
Avihai Shalom <avihais12344@gmail.com>
2024-02-11 21:15:16 +0200

    Added skeleton for earth bender tests.

--- test/test_earth_bender.py
+++ test/test_earth_bender.py
@@ -1,42 +1,142 @@
-#!/usr/bin/env python3
+from decimal import Decimal
+from logging import INFO, LogRecord
 
-import io
-import sys
+import pytest
+from hypothesis import example, given
+from hypothesis import strategies as st
 
 from tlab.earth_bender import EarthBender
+from tlab.exceptions import InvalidPowerTypeError, InvalidPowerValueError
+
+from .conftest import NEGETIVE_INTEGERS, POSITIVE_INTEGERS
 
 
-class TestEarthBender:
-    def test_has_skill_and_power(self) -> None:
-        toph = EarthBender("Toph", 95)
-        assert toph.skill == "Earthbending"
-        assert toph.power == 95
+@given(
+    name=st.text(),
+    power=POSITIVE_INTEGERS,
+)
+@example(name="Toph", power=1)
+@example(name="Toph", power=0)
+def test_ctor_sets_properties_on_valid_values(
+    name: str,
+    power: int,
+) -> None:
+    # Act.
+    aang = EarthBender(name, power)
 
-    def test_requires_int_power(self) -> None:
-        toph = EarthBender("Toph", 95)
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        toph.power = "not an integer"
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "Power level must be an integer\n"
+    # Assert.
+    assert aang.skill == "Airbending"
+    assert aang.name == name
+    assert aang.power == power
 
-    def test_can_use_earthbending(self) -> None:
-        toph = EarthBender("Toph", 95)
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        toph.use_earthbending()
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "Toph is using her earthbending skill!\n"
 
-    def test_enhance_power(self) -> None:
-        toph = EarthBender("Toph", 95)
-        toph.enhance_power(20)
-        assert toph.power == 115
+@given(
+    power=NEGETIVE_INTEGERS,
+)
+def test_ctor_requires_power_of_positive_int_and_fails_on_negetive_int(
+    power: int,
+) -> None:
+    # Act & Assert.
+    with pytest.raises(
+        InvalidPowerValueError,
+        match="Power level must be a positive integer",
+    ):
+        EarthBender("Toph", power)
 
-    def test_special_ability(self) -> None:
-        toph = EarthBender("Toph", 95)
-        special_ability_result = toph.special_ability()
-        assert (
-            special_ability_result
-            == "Toph uses seismic sense to detect hidden objects!"
-        )
+
+@given(
+    power=st.one_of(
+        st.booleans(),
+        st.floats(),
+        st.decimals(),
+        st.text(),
+    ),
+)
+@example(power=True)
+@example(power=False)
+def test_ctor_requires_power_of_positive_int_and_failes_on_non_int(
+    power: str | bool | float | Decimal,
+) -> None:
+    # Act & Assert.
+    with pytest.raises(
+        InvalidPowerTypeError,
+        match="Power level must be a positive integer",
+    ):
+        EarthBender("Toph", power)
+
+
+@given(
+    new_name=st.text(),
+)
+def test_name_setter_on_valid_value(
+    new_name: str,
+) -> None:
+    # Arrange.
+    aang = EarthBender("Toph", 90)
+
+    # Act.
+    aang.name = new_name
+
+    # Assert.
+    assert aang.name == new_name
+
+
+@given(
+    new_power=POSITIVE_INTEGERS,
+)
+def test_power_setter_on_valid_value(
+    new_power: int,
+) -> None:
+    # Arrange.
+    aang = EarthBender("Toph", 90)
+
+    # Act.
+    aang.power = new_power
+
+    # Assert.
+    assert aang.power == new_power
+
+
+@given(
+    new_power=NEGETIVE_INTEGERS,
+)
+def test_power_setter_failes_on_invalid_value(
+    new_power: int,
+) -> None:
+    # Arrange.
+    aang = EarthBender("Toph", 90)
+
+    # Act & assert.
+    with pytest.raises(
+        InvalidPowerValueError,
+        match="Power level must be a positive integer",
+    ):
+        aang.power = new_power
+
+
+@given(
+    new_power=st.one_of(
+        st.booleans(),
+        st.floats(),
+        st.decimals(),
+        st.text(),
+    ),
+)
+@example(new_power=True)
+@example(new_power=False)
+def test_power_setter_failes_on_invalid_type(
+    new_power: str | bool | float | Decimal,
+) -> None:
+    # Arrange.
+    aang = EarthBender("Toph", 90)
+
+    # Act & assert.
+    with pytest.raises(
+        InvalidPowerTypeError,
+        match="Power level must be a positive integer",
+    ):
+        aang.power = new_power
+
+
+def test_can_use_airbending() -> None:
+    raise NotImplementedError("Need to implement.")
