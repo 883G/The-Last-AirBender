9835ef73898966e70ce63e0ec57a74b9d9c6a79a
Avihai Shalom <avihais12344@gmail.com>
2024-03-02 21:34:45 +0200

    Reformatted the classes and added self type annotation.

--- tlab/fire_bender.py
+++ tlab/fire_bender.py
@@ -1,10 +1,11 @@
 import random
+from typing import Self
 
 
 class FireBender:
     # Return type is None due to: https://peps.python.org/pep-0484/#the-meaning-of-annotations
     def __init__(
-        self,
+        self: Self,
         name: str,
         power: int,
         random_generator: random.Random = random,
@@ -12,24 +13,38 @@ class FireBender:
         raise NotImplementedError("You Should Implement this method")
 
     @property
-    def name(self) -> str:
+    def name(
+        self: Self,
+    ) -> str:
         raise NotImplementedError("You Should Implement this method")
 
     @name.setter
-    def name(self, name: str) -> None:
+    def name(
+        self: Self,
+        name: str,
+    ) -> None:
         raise NotImplementedError("You Should Implement this method")
 
     @property
-    def power(self) -> int:
+    def power(
+        self: Self,
+    ) -> int:
         raise NotImplementedError("You Should Implement this method")
 
     @power.setter
-    def power(self, power: int) -> None:
+    def power(
+        self: Self,
+        power: int,
+    ) -> None:
         raise NotImplementedError("You Should Implement this method")
 
     @property
-    def skill(self) -> str:
+    def skill(
+        self: Self,
+    ) -> str:
         raise NotImplementedError("You Should Implement this method")
 
-    def bend(self) -> None:
+    def bend(
+        self: Self,
+    ) -> None:
         raise NotImplementedError("You Should Implement this method")
--- tlab/water_bender.py
+++ tlab/water_bender.py
@@ -1,35 +1,43 @@
+from typing import Self
 from webbrowser import BaseBrowser
 
 
 class WaterBender:
     # Return type is None due to: https://peps.python.org/pep-0484/#the-meaning-of-annotations
     def __init__(
-        self,
+        self: Self,
         name: str,
         power: int,
-        browser: BaseBrowser | None = None
+        browser: BaseBrowser | None = None,
     ) -> None:
         raise NotImplementedError("You Should Implement this method")
 
     @property
-    def name(self) -> str:
+    def name(self: Self) -> str:
         raise NotImplementedError("You Should Implement this method")
 
     @name.setter
-    def name(self, name: str) -> None:
+    def name(
+        self: Self,
+        name: str,
+    ) -> None:
         raise NotImplementedError("You Should Implement this method")
 
     @property
-    def power(self) -> int:
-        raise NotImplementedError("You Should Implement this method")
+    def power(
+        self: Self,
+    ) -> int:
 
     @power.setter
-    def power(self, power: int) -> None:
-        raise NotImplementedError("You Should Implement this method")
+    def power(
+        self: Self,
+        power: int,
+    ) -> None:
 
     @property
-    def skill(self) -> str:
-        raise NotImplementedError("You Should Implement this method")
-
-    def bend(self) -> None:
-        raise NotImplementedError("You Should Implement this method")
+    def skill(
+        self: Self,
+    ) -> str:
+    def bend(
+        self: Self,
+    ) -> None:
